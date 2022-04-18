489eff57f42dab64c4f6122cff655e4d11ef31a0
maxconine <58146795+maxconine@users.noreply.github.com>
2022-04-18 08:16:16 -0400

    Add files via upload

---
+++ java/frc/robot/RobotContainer.java
@@ -0,0 +1,103 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.swervedrivespecialties.swervelib.SdsModuleConfigurations;
+
+//import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+//import edu.wpi.first.wpilibj2.command.FunctionalCommand;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.Button;
+import frc.robot.commands.DefaultDriveCommand;
+import frc.robot.subsystems.DrivetrainSubsystem;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final DrivetrainSubsystem m_drivetrainSubsystem = new DrivetrainSubsystem();
+  
+
+  private final GenericHID m_driver = new GenericHID(1); // for interLINK Controller
+  private final Joystick m_operator = new Joystick(2); //for operator controller
+
+
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
+  public RobotContainer() {
+    
+    // Set up the default command for the drivetrain.
+    // The controls are for field-oriented driving:
+    // Left stick Y axis -> forward and backwards movement
+    // Left stick X axis -> left and right movement
+    // Right stick X axis -> rotation
+    m_drivetrainSubsystem.setDefaultCommand(new DefaultDriveCommand(
+            m_drivetrainSubsystem, 
+            () -> getNewAxisLeft() * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
+            () -> getNewAxisRight() * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
+            () -> getNewYawRight() * DrivetrainSubsystem.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND
+    ));
+
+    // Configure the button bindings
+    configureButtonBindings();
+  }
+
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+    // Back button zeros the gyroscope
+    new Button(m_operator::getTopPressed)//TODO: set button to zero gyroscope
+            // No requirements because we don't need to interrupt anything
+            .whenPressed(m_drivetrainSubsystem::zeroGyroscope);
+
+    //        new Button(() -> -operator.getRightY() >= +0.75)
+    //.whileHeld(new ClimberMoveUpCommand(climber));
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An ExampleCommand will run in autonomous
+    return new InstantCommand();
+  }
+
+  private double getNewAxisLeft(){
+      if (m_driver.getRawAxis(1) > 0) {
+        return (m_driver.getRawAxis(1) / 0.77);
+    }
+      else //negative value
+        return (m_driver.getRawAxis(1) / 0.97);
+  }
+
+  private double getNewAxisRight(){
+      if (m_driver.getRawAxis(4) > 0) {
+        return (m_driver.getRawAxis(4) / 0.74);
+    }
+      else //negative value
+        return (m_driver.getRawAxis(4) / 0.6);
+  }
+
+  private double getNewYawRight(){
+    //TODO: set yaw endpoints
+    return 0.0;
+  }
+}
